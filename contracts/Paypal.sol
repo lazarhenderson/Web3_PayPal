ac7f9b26f4488a5ef7bf0fccaa1c2ff16dbb44f8
lazarhenderson <95lazarh@gmail.com>
2023-04-03 16:44:24 +0200

    new file:   .gitignore 	new file:   README.md 	new file:   contracts/Paypal.sol 	new file:   hardhat.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   scripts/deploy.js 	new file:   test/Lock.js

---
+++ contracts/Paypal.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol";
+
+contract Paypal {
+
+    // Define the owner of the smart contract
+    address public owner;
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+
+    // Create Struct & Mapping for request, transaction & name
+    // - Structs show what consist inside the transaction i.e. address of requestor, name of requestor, amount requested & message
+    // - Mappings allow you to see from which address the Struct is from - map addresses to structs
+    struct request {
+        address requestor;
+        uint amount;
+        string message;
+        string name;
+    }
+
+    struct sendReceive {
+        string action;
+        uint256 amount;
+        string message;
+        address otherPartyAddress;
+        string otherPartyName;
+    }
+
+    struct userName {
+        string name;
+        bool hasName;
+    }
+
+    mapping(address => userName) names; // map address to userName struct, name of mapping: names
+    mapping(address => request[]) requests; // request[] so when calling requests[address] an array of request structs will be returned
+    mapping(address => sendReceive[]) history; // sendReceive[] so when calling history[address] an array of sendReceive structs will be returned
+
+
+    // Add name to wallet address i.e. give wallet a name
+    function addName(string memory _name) public {
+
+        // Create "userName" struct and "storage" to store data inside this contract
+        // Create temporary name "newUserName" which is used to update storage values of struct
+        // note that address mapping is set by names[msg.sender]
+        userName storage newUserName = names[msg.sender];
+        newUserName.name = _name;
+        newUserName.hasName = true;
+
+    }
+
+
+    // Create a request
+    function createRequest(address user, uint256 _amount, string memory _message) public {
+
+        // Create request struct, temporarily store in memory
+        request memory newRequest;
+        newRequest.requestor = msg.sender;
+        newRequest.amount = _amount;
+        newRequest.message = _message;
+        if (names[msg.sender].hasName) {
+            newRequest.name = names[msg.sender].name;
+        }
+
+        requests[user].push(newRequest); // this pushes the newRequest struct INTO the requests[] array of user
+
+    }
+
+
+    // Pay the request
+    // _request is an int becuase its targetting the request inside requests[] array
+    function payRequest(uint256 _request) public payable {
+
+        require(_request < requests[msg.sender].length, "No such request");
+        request[] storage myRequests = requests[msg.sender]; // Temp var's: request[], myRequests to get requests array of msg.sender
+        request storage payableRequest = myRequests[_request]; // Target the specific request thats passed into this function
+
+        uint256 toPay = payableRequest.amount *  1000000000000000000; // Convert to wei, 18 decimals
+        require(msg.value == (toPay), "Pay correct amount"); // Checks to see if number of wei sent with the message matches "toPay"
+
+        payable(payableRequest.requestor).transfer(msg.value); // Takes requestor and transfers msg.value to the requestor
+
+        myRequests[_request] = myRequests[myRequests.length - 1]; // Swap last request in myRequests & swap with current _request we just paid
+        myRequests.pop(); // Now we remove the last request (one we just paid) from the myRequests array
+
+
+
+    }
+
+
+    // Get all requests sent to a User
+    
+
+    // Get all historic transactions a user has been a part of
+
+}
+
